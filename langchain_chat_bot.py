5fe7e15a6f7c9613834220c829e2d66221d54e97
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2025-03-30 03:45:21 +0530

    create a chat prompt template

--- langchain_chat_bot.py
+++ langchain_chat_bot.py
@@ -1,7 +1,25 @@
 import os
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
+
+# load api key for large language model
 
 token = os.environ["GITHUB_TOKEN"]
 
+# create a chatbot instance
 
-llm = ChatOpenAI(token=token, model_name="gpt-4o")
+llm = ChatOpenAI(temperature=0, model_name="gpt-4o")
+
+
+# create prompt template
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system" , "You are a intelligent chatbot. Answer the following questions."),
+        ("user", "{question}"),
+
+
+
+    ]
+)
